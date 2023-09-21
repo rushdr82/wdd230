e6e934b15492470c1e8028f32ffca1c7f587659b
Dustin Rush <rushdr82@gmail.com>
2023-09-20 18:47:06 -0700

    Wed Sept 20

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,5 @@
-const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+let options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
 document.getElementById('lastModified').textContent = new Date().toLocaleDateString('en-US', options);
+
+options = { year: 'numeric' };
+document.getElementById('currentYear').textContent = new Date().toLocaleDateString('en-US', options);
