043d584dbb066eaf8c4f5d6fffa81aec02b15964
Dustin Rush <rushdr82@gmail.com>
2024-03-01 22:05:59 -0800

    Chamber Join Us Form and Table v1.0

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,6 +3,10 @@ var currentTime = new Date();
 var year = currentTime.getFullYear();
 document.getElementById("year").innerHTML = year;
 
+// returns the timestamp for the form (default format)
+var timestamp = document.timestamp;
+document.getElementById("timestamp").innerHTML = timestamp;
+
 // returns the last modified date (default format)
 var lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = lastModified;
