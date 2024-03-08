ac33fd2c6b253e883f7391404dfcff18bbbe5d64
Dustin Rush <rushdr82@gmail.com>
2024-03-07 21:13:53 -0800

    Lesson09 JSON 1.0

---
+++ scripts/links.js
@@ -0,0 +1,18 @@
+const baseURL = "https://rushdr82.github.io/wdd230/";
+const linksURL = "https://rushdr82.github.io/wdd230/data/links.json";
+
+let unorderedList = document.querySelector("#activity-links");
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    //console.log(data);
+    displayLinks(data);
+}
+
+getLinks();
+
+function displayLinks() {
+
+}
+
