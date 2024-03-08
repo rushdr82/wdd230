de8e85151d970375700e803f2228ba251ed05ee5
Dustin Rush <rushdr82@gmail.com>
2024-03-08 11:33:46 -0800

    Lesson 09 Activities

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,36 @@ let unorderedList = document.querySelector("#activity-links");
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data)
-    //displayLinks(data)
+    //console.log(data)
+    displayLinks(data.lessons)
 }
+function displayLinks(lessons) {
+    lessons.forEach((lesson) => {
+        let listElement = document.createElement('li');
+        let weekHeader = document.createElement('p');
+        weekHeader.textContent = `Week ${lesson.lesson}: `
+        listElement.appendChild(weekHeader)
+        let linksList = lesson.links
+        let linksListLength = lesson.links.length;
+        // console.log(linksList.length)
+        for (i = 0; i < linksListLength; i++) {
+            let link = linksList[i]
+            if (i < linksListLength - 1) {
+                let aTag = document.createElement('a');
+                aTag.href = `${link.url}`;
+                aTag.textContent = ` ${link.title} |`
+                listElement.appendChild(aTag);
+            } else if (i = linksListLength) {
+                let aTag = document.createElement('a');
+                aTag.href = `${link.url}`;
+                aTag.textContent = ` ${link.title}`
+                listElement.appendChild(aTag);
+            }
+        }
+        unorderedList.appendChild(listElement);
+
+    });
+
+}
+
 getLinks();
