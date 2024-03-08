96c93efc5126c513a0a7b0bff19bc7de4ecc9cdf
Dustin Rush <rushdr82@gmail.com>
2024-03-07 21:19:30 -0800

    js correction

--- scripts/links.js
+++ scripts/links.js
@@ -1,18 +1,11 @@
 const baseURL = "https://rushdr82.github.io/wdd230/";
 const linksURL = "https://rushdr82.github.io/wdd230/data/links.json";
-
 let unorderedList = document.querySelector("#activity-links");
-
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    //console.log(data);
-    displayLinks(data);
+    console.log(data);
+    //displayLinks(data);
 }
-
 getLinks();
-
-function displayLinks() {
-
-}
 
